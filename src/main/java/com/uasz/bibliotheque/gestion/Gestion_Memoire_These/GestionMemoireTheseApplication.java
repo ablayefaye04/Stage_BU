d53c9b8388586c7d49aed13fd0e804bbc641f4ff
ablayefaye04 <fayeablaye404@gmail.com>
2024-12-03 22:31:58 +0000

    Initialisation du projet

---
+++ src/main/java/com/uasz/bibliotheque/gestion/Gestion_Memoire_These/GestionMemoireTheseApplication.java
@@ -0,0 +1,13 @@
+package com.uasz.bibliotheque.gestion.Gestion_Memoire_These;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMemoireTheseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionMemoireTheseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uasz/bibliotheque/gestion/Gestion_Memoire_These/GestionMemoireTheseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uasz.bibliotheque.gestion.Gestion_Memoire_These;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionMemoireTheseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
